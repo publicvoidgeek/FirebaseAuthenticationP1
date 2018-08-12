3766f21146f530e34d013956cf787963c9ac4106
sanamsuri <publicvoidgeek@sanams-macbook-pro.local>
2018-08-12 16:06:26 +0530

    Initial Commit

---
+++ FirebaseAuthentication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseAuthentication
+//
+//  Created by sanamsuri on 12/08/18.
+//  Copyright © 2018 sanamsuri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
